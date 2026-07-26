8b39317002d84fdc3daa2c447e171534513e2913
Dominik Weber <weberdomi@gmail.com>
2026-07-26 15:10:31 +0200

    hassfest error

--- custom_components/ford_triplog/const.py
+++ custom_components/ford_triplog/const.py
@@ -26,7 +26,7 @@ from typing import Final
 DOMAIN: Final = "ford_triplog"
 NAME: Final = "Ford Triplog"
 GENERATOR: Final = "Ford Triplog"
-VERSION: Final = "1.6.2a"
+VERSION: Final = "1.6.2"
 
 #
 # Storage
