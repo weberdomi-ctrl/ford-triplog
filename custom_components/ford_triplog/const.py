fae7554f59d497f94bcecfb39bd734432d3d1d16
Dominik Weber <weberdomi@gmail.com>
2026-08-15 11:38:41 +0200

    Update const.py

--- custom_components/ford_triplog/const.py
+++ custom_components/ford_triplog/const.py
@@ -18,7 +18,7 @@ from typing import Final
 DOMAIN: Final = "ford_triplog"
 NAME: Final = "Ford Triplog"
 GENERATOR: Final = "Ford Triplog"
-VERSION: Final = "2.0.3"
+VERSION: Final = "2.1.0"
 
 #
 # Storage
