77a404d6706d82b8b54443a019da1d539c1f02ba
Dominik Weber <weberdomi@gmail.com>
2026-09-17 16:59:50 +0200

    Build: 24001

--- custom_components/ford_triplog/const.py
+++ custom_components/ford_triplog/const.py
@@ -18,8 +18,8 @@ from typing import Final
 DOMAIN: Final = "ford_triplog"
 NAME: Final = "Ford Triplog"
 GENERATOR: Final = "Ford Triplog"
-VERSION: Final = "2.4.2"
-BUILD: Final = "24203"
+VERSION: Final = "2.4.0"
+BUILD: Final = "24001"
 
 #
 # Storage
